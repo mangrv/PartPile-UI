2cf15a52a665de4867ebe3cfc21f0bdc4b53574e
ji <developer@mangrv.com>
2023-11-22 17:45:12 -0500

    DEV-42 Updating the login.js file to use auth context and to match ui design

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,22 +1,27 @@
+// src/components/auth/Login.js
+
 import { h } from 'preact';
 import { useState } from 'preact/hooks';
-import { auth } from '../../config/firebaseConfig';
+import { useAuth } from '../../context/AuthContext'; // Import useAuth
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { currentUser, signOut } = useAuth(); // Use the auth context
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      console.log('Logged in');
-    } catch (error) {
-      console.error(error.message);
-    }
+    // Logic to handle login
+    // This will depend on your Firebase setup and the methods you've provided
   };
 
-  return (
+  // If a currentUser exists, display a logout button instead of the login form
+  return currentUser ? (
+    <div>
+      <h1>Welcome, {currentUser.email}</h1>
+      <button onClick={signOut}>Logout</button>
+    </div>
+  ) : (
     <form onSubmit={handleSubmit}>
       <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
       <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
